cab12e6a47be3a66f49a8fb74c87c84a90c548c3
PCMS-SPMNET3 <78403871+PCMS-SPMNET3@users.noreply.github.com>
2022-10-13 11:15:48 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(5)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    count = 9
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
 })
 basic.forever(function () {
 	
